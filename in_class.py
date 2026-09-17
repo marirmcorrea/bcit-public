b1b23ca096e925db5aaf78bfc7d73538e9e5edfb
Mariana Correa <mari.rmcorrea@gmail.com>
2026-09-17 16:01:18 -0700

    Add files via upload

---
+++ in_class.py
@@ -0,0 +1,17 @@
+x = 10
+y = "M"
+z = float(10.06)
+
+x = "D"
+y = True;
+z = float(26.03)
+
+print(x)
+print(type(y))
+
+X = "CIT"
+Y = "1515"
+print("A" + X + Y)
+
+assigment_number = "2"
+print(f'Assigment {assigment_number} created')
